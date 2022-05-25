54018bd105fe916ee880dc9bdf363d6ca7cef17d
tranthao9 <tt088038@gmial.com>
2022-05-26 05:06:02 +0700

    finish

--- Js/Js/dongMT.js
+++ Js/Js/dongMT.js
@@ -19,13 +19,16 @@ function ADDDongMT()
     var file=$('#hinhanh').val();
     var number=$('#soluongDMT').val();
     var kt=true;
+    var stt=0;
     if(listDMT == "")
     {
         mad=1;
+        stt=1;
     }
     else
     {
         mad=listDMT[listDMT.length-1]['madong']+1;
+        stt=listDMT.length+1;
     }
     if(tend==""  )
     {
@@ -44,7 +47,9 @@ function ADDDongMT()
     }
     if(kt==true)
     {
+        $('.note').text("*")
         var dong={
+            'stt':stt,
             'madong':mad,
             "tendong":tend,
             "motad":motad,
@@ -53,7 +58,7 @@ function ADDDongMT()
         }
         listDMT.push(dong)
         localStorage.setItem("dongmt",JSON.stringify(listDMT));
-        LoadDongMT();
+        LoadDongMT(listDMT)
        RefreshD();
         alert("Bạn đã thêm thành công") ; 
     }
@@ -71,14 +76,15 @@ function RefreshD(){
         $('#madong').val("D"+(Number(listDMT[listDMT.length-1]['madong'])+1));
     $('#tendong').val("");
     $('#motad').val("");
-    $('#soluongDT').val("1")
+    $('#soluongDMT').val("1")
+    $('#hinhanh').val("");
 }
-function LoadDongMT()
+function LoadDongMT(listDMT)
 {
     
     var stt=0;
     var strrr=`<tr>
-            <th><input type="checkbox"></th>
+            <th><input type="checkbox" onclick="CheckAll()"></th>
             <th>STT</th>
             <th>Mã dòng máy tính</th>
             <th>Tên dòng máy tính</th>
@@ -95,8 +101,8 @@ function LoadDongMT()
         if(stt<10)
         {
             strr += `<tr>
-            <td><input type="checkbox"></td>
-            <td>`+stt+`</td>
+            <td><input class="input2" type="checkbox" name="check_all"></td>
+            <td>`+x.stt+`</td>
             <td> D0`+x.madong+`</td>
             <td>`+x.tendong+`</td>
             <td><img src="image/`+(x.hinhanh).slice(a+1,x.hinhanh.length)+`" width="80px"></img></td>
@@ -108,8 +114,8 @@ function LoadDongMT()
         else
         {
             strr += `<tr>
-            <td><input type="checkbox"></td>
-            <td>`+stt+`</td>
+            <td><input class="input2" type="checkbox" name="check_all"></td>
+            <td>`+x.stt+`</td>
             <td> D`+x.madong+`</td>
             <td>`+x.tendong+`</td>
             <td><img src="image/`+(x.hinhanh).slice(a+1,x.hinhanh.length)+`" width="80px"></img></td>
@@ -123,20 +129,36 @@ function LoadDongMT()
     var str = strrr+ strr;
     $("#adddongMT").html(str);
 }
-LoadDongMT();
+LoadDongMT(listDMT)
 
 function RemoveDongMT(ma){
     var index=listDMT.findIndex( x => x.madong == ma);
   if(index >=0 ){
     listDMT.splice(index,1);
   }
-  LoadDongMT();
+  
+
+  LoadDongMT(listDMT)
+}
+function RemoveSTT(ma)
+{
+    var index=listDMT.findIndex( x => x.stt== ma);
+  if(index >=0 ){
+    listDMT.splice(index,1);
+  }
+  for(var i=0;i<listDMT.length;i++)
+    {
+        listDMT[i].stt=i+1;
+    }
+  localStorage.setItem('dongmt',JSON.stringify(listDMT));
+ 
 }
 function UpdateDongMT()
 {
     localStorage.setItem('dongmt',JSON.stringify(listDMT));
     alert("Bạn đã cập nhập thành công");
-    LoadDongMT();
+    RefreshD();
+    LoadDongMT(listDMT)
 }
 function EditDongMT(ma)
 {
@@ -145,7 +167,6 @@ function EditDongMT(ma)
     $('#madong').val(listDMT[index]['madong']);
     $('#tendong').val(listDMT[index]['tendong']);
     $('#motad').val(listDMT[index]['motad']);
-    $('#hinhanh').val(listDMT[index]['hinhanh']);
     $('#soluongDMT').val(listDMT[index]['soluongDT']);
 }
 function UpdateDongMTT()
@@ -155,6 +176,10 @@ function UpdateDongMTT()
     var file=$('#hinhanh').val();
     var soluong=$('#soluongDMT').val()
     var ok=true;
+    for(var i=0;i<listDMT.length;i++)
+    {
+        listDMT.stt=i+1;
+    }
     if(ten==""  )
     {
         ok=false
@@ -167,6 +192,7 @@ function UpdateDongMTT()
     }
     if(ok==true)
     {
+        $('.note').text("*")
         for(x of listDMT)
         {
             if(x.madong==$('#madong').val())
@@ -192,7 +218,83 @@ function UpdateDongMTT()
     {
         alert("Dữ liệu không được để trống")
     }
-
+}
+function CheckAll() {
+    var ids = document.getElementsByClassName("input2");
+    for (var i = 0; i < ids.length; i++) {
+        if (ids[i].name == "check_all") {
+            ids[i].checked = true;
+        }
+    }
+}
+function XoaAll()
+{
+    var ids = document.getElementsByClassName("input2");
+    var i=0
+    var x=ids.length;
+    while(i < ids.length) {
+        for(var z=0;z<x;z++)
+        {
+            if (ids[z].checked  == true ) {
+                ids[z].className=ids[z].className.replace("input2","")
+                RemoveSTT(z+1);
+                x=x-1;
+                break;
+            }
+        }
+        i=i+1;
+    }
+    LoadDongMT(listDMT)
     
-   
+}
+function Search()
+{
+    if($('#textDMTMa').val() != "" && $('#textDMTT').val()=="")
+    {
+        var listNew = []
+        for(var i =0;i<listDMT.length;i++)
+        {
+            if( $('#textDMTMa').val().includes(String(listDMT[i].madong))==true)
+            {
+                listNew.push(listDMT[i])
+                break;
+            }
+            
+        }
+        LoadDongMT(listNew)
+    }
+    else if($('#textDMTMa').val() == "" && $('#textDMTT').val() !="")
+    {
+        var listNew2 = [];
+        for(var i =0;i<listDMT.length;i++)
+        {
+            if($('#textDMTT').val().includes(String(listDMT[i].tendong))==true)
+            {
+                
+                listNew2.push(listDMT[i])
+            }
+            
+        }
+        LoadDongMT(listNew2)
+    }
+    else if($('#textDMTMa').val() != "" && $('#textDMTT').val() !="")
+    {
+        var listNew3 = [];
+        for(var i =0;i<listDMT.length;i++)
+        {
+            if($('#textDMTT').val().includes(String(listDMT[i].tendong))==true && 
+            $('#textDMTMa').val().includes(String(listDMT[i].madong))==true)
+            {
+               
+                listNew3.push(listDMT[i])
+                break;
+            }
+            
+        }
+        LoadDongMT(listNew3)
+    }
+    else
+    {
+        LoadDongMT(listDMT);
+    }
 }
